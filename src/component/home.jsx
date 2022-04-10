f337f75efdbdbad1af67d4454cb77ef1fbc06b03
M Waqar <m.waqar.c320@gmail.com>
2022-04-10 17:44:46 +0500

    last changes

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -5,17 +5,13 @@ import {products} from './../apiData.js';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 const Home = () => {
-const {noOfCartItem,setNoOfCartItem,savedProductData,dispatchProductData}=useContext(CreateContext);
-useEffect(()=>{
-
-console.log(JSON.stringify(savedProductData))
-},[savedProductData])
+const {noOfCartItem,setNoOfCartItem,savedProductData,dispatchProductData}=useContext(CreateContext)
 return <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Cart App </a>
     <form class="d-flex">
-        <Link to="/cart" class="btn success" style={{background:'black',color:'white'}} type="submit">cart items ({noOfCartItem})</Link>
+        <Link to="/cart" class="btn success" style={{background:'black',color:'white'}} type="submit">cart items ({savedProductData.length})</Link>
       </form>
 
   </div>
@@ -32,7 +28,7 @@ return <div>
   <div class="card-body">
     <h5 class="card-title">{pro.name}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button onClick={()=>{setNoOfCartItem(noOfCartItem+1);dispatchProductData({type:"add",payload:{id:pro.id,name:pro.name,img:pro.pic}})}} class="btn btn-primary">add to cart</button>
+    <button onClick={()=>{dispatchProductData({type:"add",payload:{id:pro.id,name:pro.name,img:pro.pic}})}} class="btn btn-primary">add to cart</button>
   </div>
 </div>
   ))
